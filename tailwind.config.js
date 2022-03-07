7387f9c1843e87d7ab49554b749afb61643fbcbe
matheussousaf <matheussousaf.js@gmail.com>
2022-03-07 16:06:52 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+        "./src/layouts/**/*.{js,ts,jsx,tsx}",
+        "./src/screens/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["IBM Plex Sans", "sans-serif"],
+                mono: ["Fira Mono", "mono"],
+            },
+            borderWidth: {
+                1: "1px",
+            },
+            colors: {
+                "theme-black": "#13151b",
+                "theme-blue": "#101528",
+                "theme-red": "#1C0300",
+                "theme-gray-100": "#9EA3B1",
+                "theme-gray-300": "#393F51",
+                "theme-gray-400": "#353943",
+                "theme-gray-900": "#232323",
+                "theme-light-gray": "#7C7C7C",
+                "theme-green": "#70D756",
+                "theme-discord": "#5765EE",
+            },
+        },
+    },
+    plugins: [],
+};
