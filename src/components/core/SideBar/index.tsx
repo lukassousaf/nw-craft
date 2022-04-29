97caa829be7d385d6644fbdeefe07f59747d80b0
matheussousaf <matheussousaf.js@gmail.com>
2022-04-29 18:32:58 -0300

    feat: Adding Sidebar

--- src/components/core/SideBar/index.tsx
+++ src/components/core/SideBar/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SideBar: React.FC = ({ children }) => {
   return (
-    <div className="   flex flex-col  absolute  w-1/4 h-full top-0 right-0  bg-theme-gray-200">
+    <div className="flex flex-col absolute w-1/4 h-full top-0 right-0 bg-theme-gray-200">
       {children}
     </div>
   );
